0ff77f9f80d1937dda64d9cf440a46fe2b8e7443
anrbl <133613567+anrbl@users.noreply.github.com>
2023-08-08 16:52:29 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -13,4 +13,24 @@ $(function () {
     $('.notice_arrow .next').on('click', function () {
         $('.notice_slide').slick('slickNext');
     })
+
+
+    $('.doc_title li').on('click', function (e) {
+        e.preventDefault();
+
+        let idx = $(this).index();
+        $('.doc_title li').eq(idx).addClass('on').siblings().removeClass('on');
+        $('.doc_center>li').eq(idx).addClass('on').siblings().removeClass();
+    });
+
+
+    var mapContainer = $('#map')[0]
+    var mapOption = {
+        center: new kakao.maps.LatLng(37.53257409719912, 126.7367517389714),
+        level: 3,
+    };
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+    $(function () {
+        kakaoMap();
+    })
 });
