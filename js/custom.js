1dd06139cf0e2e5e291f58664fe31c81209c4fe4
anrbl <133613567+anrbl@users.noreply.github.com>
2023-08-09 18:18:19 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,24 @@
 $(function () {
+    //헤더
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
+    });
+    $('.to_top').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 600);
+    });
+
+    //메인 비주얼 슬라이드 - 페이드 모드 (main>seciton.main_visual>.main_slide)
     $('.main_slide').slick({
         arrows: false,
         fade: true,
+        autoplay: true,
+        autoplaySpeed: 4500,
+        dots: true,
     });
 
+
+    // 공지사항 슬라이드 - 버티컬 모드 (main>section.main_visual>.notice_slide)
     $('.notice_slide').slick({
         arrows: false,
         vertical: true,
@@ -16,6 +31,7 @@ $(function () {
     })
 
 
+    //진료과 소개 탭메뉴(main>section.main_doctor.doctor_right>.doctor_title)
     $('.doc_title li').on('click', function (e) {
         e.preventDefault();
 
@@ -24,8 +40,9 @@ $(function () {
         $('.doc_center>li').eq(idx).addClass('on').siblings().removeClass();
     });
 
+    //패밀리사이트(footer>ft>foot_family)
     $('.foot_family span').on('click', function () {
-        $('.family_list').toggleClass('on');
+        $('.foot_family ul').toggleClass('on');
     })
 });
 
@@ -37,9 +54,13 @@ $(function () {
 var mapContainer = document.getElementById('map');
 var mapOptions = {
     center: new kakao.maps.LatLng(37.532574900, 126.736850),
+    draggable: false,
     level: 2,
 };
 var map = new kakao.maps.Map(mapContainer, mapOptions);
+function setZoomable(zoomable) {
+    map.setZoomable(zoomable);
+}
 
 //마커표시
 var markerPosition = new kakao.maps.LatLng(37.532574900, 126.736850);
@@ -50,10 +71,11 @@ marker.setMap(map);
 
 //마커 위 텍스트 
 var mkOnText = document.querySelector('.main_map .desc')
-var mkOnPosition = new kakao.maps.LatLng(37.532574900, 126.736850);
+//var mkOnPosition = new kakao.maps.LatLng(37.532574900, 126.736850);
 var infowindow = new kakao.maps.InfoWindow({
-    position: mkOnPosition,
+    //position: mkOnPosition,
     content: mkOnText
 });
 
-infowindow.open(map, marker); 
+infowindow.open(map, marker);
+
